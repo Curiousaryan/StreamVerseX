f9c4fd98e383b454f244400f841ec594fe7ddb66
ypardeshi561 <ypardeshi561@gmail.com>
2026-07-21 23:12:46 +0530

    chore: initial Spring Boot 4 project setup with CORS, Security, and Swagger config skeleton

---
+++ streamversex-backend/src/main/java/com/streamversex/backend/StreamverseXApplication.java
@@ -0,0 +1,13 @@
+package com.streamversex.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamverseXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamverseXApplication.class, args);
+	}
+
+}
---
+++ streamversex-backend/src/test/java/com/streamversex/streamversex_backend/StreamversexBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.streamversex.streamversex_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamversexBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
